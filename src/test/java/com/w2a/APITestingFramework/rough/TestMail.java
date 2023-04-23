9bb99ff5fd3a7d50ecdf288c07a1eb61168f21d7
Arsh Gupta <arshgupta@Arsh-Guptas-Mac.local>
2023-04-23 17:10:37 +0530

    fifth commit

---
+++ src/test/java/com/w2a/APITestingFramework/rough/TestMail.java
@@ -0,0 +1,22 @@
+package com.w2a.APITestingFramework.rough;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import com.w2a.APITestingFramework.utilities.MonitoringMail;
+import com.w2a.APITestingFramework.utilities.TestConfig;
+
+
+public class TestMail {
+	static String messageBody;
+	public static void main(String[] args) throws AddressException, MessagingException, UnknownHostException {
+
+		messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()+":8080/job/APITestingFramework/Extent_20Reports/";
+
+		MonitoringMail mail = new MonitoringMail();
+		mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+	}
+}
